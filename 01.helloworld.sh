4d9e75edede08b9b8f14f21e34be764e6ab69005
kikilion <2636944771@qq.com>
2019-10-12 10:57:06 +0800

    write hello world file

--- 01.helloworld.sh
+++ 01.helloworld.sh
@@ -3,5 +3,8 @@
 # filepath: jiling/01.helloworld.sh
 # email: 2636944771@qq.com
 # outhor: kikilion
-# date: 2019/10/12
+# date: 2019/10/12/10-24
+# modify_time: 2019/10/12/10-56
+# usage: print hello world text.
+
 echo 'hello world'
